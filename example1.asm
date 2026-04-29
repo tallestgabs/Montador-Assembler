5989618516a042a7427e320ad300a04594cc5fcd
tallestgabs <gabrielcastrodias1414@gmail.com>
2026-04-29 16:16:16 -0300

    create example

---
+++ example1.asm
@@ -0,0 +1,17 @@
+SECtIOn       DATA
+                        B: 
+ SPaCE  
+ H: SpACE 
+r:     SPACE 
+DoIS:           CONST 
+     2
+section TeXT
+InPUT         B
+INpUt               h
+LOAD   B
+MUL       H
+DIV    DOis 
+STORE       R
+                    OUTPUT   R
+    STOP
+    
